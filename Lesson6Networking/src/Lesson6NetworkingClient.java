ebc7d78434edf96b97eaeb62713ead3666a9f251
dresanchez-sammy <andreasesanchez17@gmail.com>
2019-11-05 08:17:39 -0800

    almost done

--- Lesson6Networking/src/Lesson6NetworkingClient.java
+++ Lesson6Networking/src/Lesson6NetworkingClient.java
@@ -3,5 +3,31 @@ import java.net.*;
 import java.util.*;
 
 public class Lesson6NetworkingClient {
+	public static void main(String[] args) throws IOException {
+		
+		String host;
+		int port;
+		
+		if (args.length == 4) {
+			host = args[1];
+			port = Integer.parseInt(args[3]);
+		}
+		else {
+			port = 8080;
+			host = "127.0.0.1";
+		}
 
+		try{
+			Socket s = new Socket(host, port);
+			Scanner in = new Scanner(s.getInputStream(), "UTF-8");
+			System.out.println("waiting...");
+			while(in.hasNextLine()) {
+				String line = in.nextLine();
+				System.out.println(line);
+			}
+		}
+		finally {
+			System.out.println("Done");
+		}
+	}
 }
--- Lesson6Networking/src/Lesson6NetworkingServer.java
+++ Lesson6Networking/src/Lesson6NetworkingServer.java
@@ -3,7 +3,32 @@ import java.net.*;
 import java.util.*;
 
 public class Lesson6NetworkingServer {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		int port;
+		if (args.length == 2) {
+			port = Integer.parseInt(args[1]);
+		}
+		else {
+			port = 8080;
+		}
+		System.out.println("Server running on port: " + port);
 		
+		try (ServerSocket s = new ServerSocket(port)){
+			System.out.println("Waiting for connection...");
+			try(Socket incoming = s.accept()){
+				System.out.println("Connected");
+				InputStream inStream = incoming.getInputStream();
+				OutputStream outStream = incoming.getOutputStream();
+				
+				try(PrintWriter out = new PrintWriter(
+						new OutputStreamWriter(outStream,"UTF-8"), true)) {
+					out.println("<html>");
+					out.println("<head><title>Java Networking</title></head>");
+					out.println("<body>");
+					out.println("<h1>Java Networking</h1>");
+					out.println("</html>");
+				}
+			}
+		}
 	}
 }
